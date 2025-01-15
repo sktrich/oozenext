7b191e65760ff3fe12c4c9c76355f6bd1cfc2f84
sktrich <r.arumakan@gmail.com>
2025-01-15 16:13:08 -0300

    news

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,78 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { lusitana } from "./ui/fonts";
+import { text } from "stream/consumers";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className={`${lusitana.className} bg-h-opage-h text-sm`}>
+      <section id="home">
+        <header className="bg-h-blue-p  shadow-h-black-u h-24 flex py-3 px-5 justify-between items-center sticky top-0 z-20 duration-dur-s">
+          <div className="flex justify-center cursor-pointer text-6xl">
+            <span
+              className="absolute rounded-full border-2 border-border-style w-24 h-24 top-2"
+              style={{
+                backgroundImage:
+                  "linear-gradient(45deg, rgb(191, 28, 28, 75%), rgb(204, 110, 110, 75%))",
+              }}
+            ></span>
+            <h1>
+              <a
+                href="#home"
+                className="relative no-underline text-h-link-t "
+                style={{
+                  letterSpacing: "14px",
+                  textShadow:
+                    "-3px -3px 0px rgb(16, 15, 15, 0.953), -3px 3px 0px rgb(16, 15, 15, 0.953), 3px -3px 0px rgb(16, 15, 15, 0.953), 3px 0px 0px rgb(16, 15, 15, 0.953)",
+                }}
+              >
+                HAJIMARI
+              </a>
+            </h1>
+          </div>
+          <div className="container">
+            <nav className="hidden md:flex items-center gap-10 text-md">
+              <ul class="nav-list">
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#sobre">Sobre</a>
+                </li>
+                <li>
+                  <a href="#produtos">Produtos</a>
+                </li>
+                <li>
+                  <a href="#contato">Contato</a>
+                </li>
+                <li>
+                  <a href="#depoimentos">depoimentos</a>
+                </li>
+              </ul>
+            </nav>
+            <div class="toggle-switch">
+              <label class="switch-label">
+                <input type="checkbox" class="checkbox" />
+                <span class="slider"></span>
+              </label>
+            </div>
+            <label class="hamburger">
+              <input type="checkbox" />
+              <svg viewBox="0 0 32 32">
+                <path
+                  class="line line-top-bottom"
+                  d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                ></path>
+                <path class="line" d="M7 16 27 16"></path>
+              </svg>
+            </label>
+          </div>
+        </header>
+        <h1>HAJIMARI</h1>
+      </section>
+      <section>
+        <h1>Minhas gotas evaporam</h1>
+        <p>auhsuasa husjaijsi ajijsidj as</p>
+      </section>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        "h-blue-p": "rgb(40, 23, 83)",
+        "font-body": "rgb(15, 15, 16)",
+        "h-red-s": "rgb(191, 28, 28, 75%)",
+        "border-style": "rgb(204, 110, 110, 75%)",
+
+        "h-link-t": "rgb(240, 248, 255)",
+        "h-link-h": "rgb(100, 159, 210)",
+        "h-black-u": "rgb(16, 15, 15, 0.953)",
+
+        "h-page-o": "rgb(211, 85, 148)",
+        "h-opage-h": "rgb(240, 248, 255)",
+        "cor-be": "rgb(183, 22, 36)",
+        "cor-ve": "rgb(255, 255, 255)",
+      },
+
+      boxShadow: {
+        "black-u": "0px 9px 10px var(--bg-h-black-u)",
+      },
+
+      transitionDuration: {
+        "dur-s": "0.7s",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
