007f244aea93bdb2cd185894a789d39131442de7
sktrich <r.arumakan@gmail.com>
2025-01-15 17:23:21 -0300

    Correção do nome das className

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           </div>
           <div className="container">
             <nav className="hidden md:flex items-center gap-10 text-md">
-              <ul class="nav-list">
+              <ul className="nav-list">
                 <li>
                   <a href="#home">Home</a>
                 </li>
@@ -49,20 +49,20 @@ export default function Home() {
                 </li>
               </ul>
             </nav>
-            <div class="toggle-switch">
-              <label class="switch-label">
-                <input type="checkbox" class="checkbox" />
-                <span class="slider"></span>
+            <div className="toggle-switch">
+              <label className="switch-label">
+                <input type="checkbox" className="checkbox" />
+                <span className="slider"></span>
               </label>
             </div>
-            <label class="hamburger">
+            <label className="hamburger">
               <input type="checkbox" />
               <svg viewBox="0 0 32 32">
                 <path
-                  class="line line-top-bottom"
+                  className="line line-top-bottom"
                   d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
                 ></path>
-                <path class="line" d="M7 16 27 16"></path>
+                <path className="line" d="M7 16 27 16"></path>
               </svg>
             </label>
           </div>
